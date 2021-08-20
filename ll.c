b90ac5d548c223e8a491d1e53374bfdcd7f631af
Aidan Evans <38838579+nsnave@users.noreply.github.com>
2021-08-20 04:02:09 -0400

    fixed types

--- ll.c
+++ ll.c
@@ -4,33 +4,25 @@
 
 #include "ll.h"
 
-/*
-typedef struct DoubleNode {
-  void * data;
-  struct DoubleNode * prev;
-  struct DoubleNode * next;
-} DoubleNode;
-*/
-
 /*** Utility Functions ***/
 
-void ll_print_nodes(Node * head) {
-  Node * current_node = head;
+void ll_print_nodes(NodeLL * head) {
+  NodeLL * current_node = head;
   while (current_node != NULL) {
     printf("%d\n", current_node->data);
     current_node = current_node->next;
   }
 }
 
-Node * ll_create_node(int new_data) {
-  Node * new = malloc(sizeof(Node));
+NodeLL * ll_create_node(int new_data) {
+  NodeLL * new = malloc(sizeof(Node));
   new->data = new_data;
   return new;
 }
 
-Node ** ll_find_node_ptr(Node ** head_ptr, Node * to_find) {
+NodeLL ** ll_find_node_ptr(NodeLL ** head_ptr, NodeLL * to_find) {
   // Finds the location of the node to_find in the list
-  Node ** current_node = head_ptr;
+  NodeLL ** current_node = head_ptr;
   while (*current_node != NULL) {
     if ((*current_node) == to_find) break;
     current_node = &((*current_node)->next);
@@ -41,10 +33,10 @@ Node ** ll_find_node_ptr(Node ** head_ptr, Node * to_find) {
 
 /*** Delete Node Function ***/
 
-void ll_delete_node(Node ** head_ptr, Node * to_delete) {
-  Node ** current_node = ll_find_node_ptr(head_ptr, to_delete); // *current_node == to_delete
+void ll_delete_node(NodeLL ** head_ptr, NodeLL * to_delete) {
+  NodeLL ** current_node = ll_find_node_ptr(head_ptr, to_delete); // *current_node == to_delete
   if (*current_node != NULL) {
-    Node * next = (*current_node)->next;
+    NodeLL * next = (*current_node)->next;
     free(*current_node);
     *current_node = next;
   }
@@ -53,26 +45,26 @@ void ll_delete_node(Node ** head_ptr, Node * to_delete) {
 
 /*** Insertion Node Functions ***/
 
-void ll_insert_node_front(Node ** head_ptr, Node * to_insert) {
+void ll_insert_node_front(NodeLL ** head_ptr, NodeLL * to_insert) {
   to_insert->next = *head_ptr;
   *head_ptr = to_insert;
 }
 
-void ll_insert_node_before(Node ** head_ptr, Node * to_insert, Node * key_node) {
-  Node ** current_node = ll_find_node_ptr(head_ptr, key_node);  // *current_node == key_node
+void ll_insert_node_before(NodeLL ** head_ptr, NodeLL * to_insert, NodeLL * key_node) {
+  NodeLL ** current_node = ll_find_node_ptr(head_ptr, key_node);  // *current_node == key_node
   to_insert->next = *current_node;
   *current_node = to_insert;
 }
 
-void ll_insert_node_after(Node ** head_ptr, Node * to_insert, Node * key_node) {
-  Node ** current_node = ll_find_node_ptr(head_ptr, key_node);  // *current_node == key_node
+void ll_insert_node_after(NodeLL ** head_ptr, NodeLL * to_insert, NodeLL * key_node) {
+  NodeLL ** current_node = ll_find_node_ptr(head_ptr, key_node);  // *current_node == key_node
   to_insert->next = (*current_node)->next;
   (*current_node)->next = to_insert;
 }
 
-void ll_insert_node_sorted(Node ** head_ptr, Node * to_insert) {
+void ll_insert_node_sorted(NodeLL ** head_ptr, NodeLL * to_insert) {
   // Finds the location to insert the node
-  Node ** current_node = head_ptr;
+  NodeLL ** current_node = head_ptr;
   while (*current_node != NULL) {
     if (to_insert->data <= (*current_node)->data) break;
     current_node = &((*current_node)->next);
@@ -86,23 +78,23 @@ void ll_insert_node_sorted(Node ** head_ptr, Node * to_insert) {
 
 /*** Insertion Data Functions ***/
 
-void ll_insert_data_front(Node ** head_ptr, int new_data) {
-  Node * new = ll_create_node(new_data);
+void ll_insert_data_front(NodeLL ** head_ptr, int new_data) {
+  NodeLL * new = ll_create_node(new_data);
   ll_insert_node_front(head_ptr, new);
 }
 
-void ll_insert_data_before(Node ** head_ptr, int new_data, Node * key_node) {
-  Node * new = ll_create_node(new_data);
+void ll_insert_data_before(NodeLL ** head_ptr, int new_data, NodeLL * key_node) {
+  NodeLL * new = ll_create_node(new_data);
   ll_insert_node_before(head_ptr, new, key_node);
 }
 
-void ll_insert_data_after(Node ** head_ptr, int new_data, Node * key_node) {
-  Node * new = ll_create_node(new_data);
+void ll_insert_data_after(NodeLL ** head_ptr, int new_data, NodeLL * key_node) {
+  NodeLL * new = ll_create_node(new_data);
   ll_insert_node_after(head_ptr, new, key_node);
 }
 
-void ll_insert_data_sorted(Node ** head_ptr, int new_data) {
-  Node * new = ll_create_node(new_data);
+void ll_insert_data_sorted(NodeLL ** head_ptr, int new_data) {
+  NodeLL * new = ll_create_node(new_data);
   ll_insert_node_sorted(head_ptr, new);
 }
 
@@ -110,7 +102,7 @@ void ll_insert_data_sorted(Node ** head_ptr, int new_data) {
 /*** Main ***/
 
 void main() {
-  Node * head = NULL;
+  NodeLL * head = NULL;
   ll_insert_data_sorted(&head, 1337);
   ll_insert_data_sorted(&head, 1500);
   ll_insert_data_sorted(&head, 1111);
